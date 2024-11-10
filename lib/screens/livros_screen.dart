a00aa798be1346dbcf7b3fc0c09cfa74cc854f2e
EnzoLavieri <77125886+EnzoLavieri@users.noreply.github.com>
2024-11-10 18:58:36 -0300

    show autor name on listaLivro

--- lib/screens/livros_screen.dart
+++ lib/screens/livros_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/livros/livro.dart';
+import '../models/autores/autor.dart';
 import '../services/api_service.dart';
 import 'formulario_livro_screen.dart';
 
@@ -11,27 +12,42 @@ class LivrosScreen extends StatefulWidget {
 }
 
 class _LivrosScreenState extends State<LivrosScreen> {
-  late Future<List<Livro>> livros;
-  final ApiService apiService = ApiService();
+  List<Autor> autores = [];
+
+  Future<List<Livro>>? livros;
 
   @override
   void initState() {
     super.initState();
-    livros = apiService.fetchLivros();
+    _loadLivrosAndAutores();
   }
 
-  void _toggleFavorito(Livro livro) async {
-    setState(() {
-      livro.isFavorito = !livro.isFavorito;
-    });
-    await apiService.updateLivro(livro);
+  Future<void> _loadLivrosAndAutores() async {
+    try {
+      final fetchedAutores = await ApiService().fetchAutores();
+      final fetchedLivros = ApiService().fetchLivros();
+      setState(() {
+        autores = fetchedAutores;
+        livros = fetchedLivros;
+      });
+    } catch (error) {
+      print("Erro ao carregar livros ou autores: $error");
+    }
+  }
+
+  String _getAutorNome(int autorId) {
+    final autor = autores.firstWhere(
+      (autor) => int.tryParse(autor.id) == autorId,
+      orElse: () => Autor(id: '', nome: 'Desconhecido', nacionalidade: ''),
+    );
+    return autor.nome;
   }
 
   void _deleteLivro(String livroId) async {
     try {
-      await apiService.deleteLivro(livroId);
+      await ApiService().deleteLivro(livroId);
       setState(() {
-        livros = apiService.fetchLivros();
+        livros = ApiService().fetchLivros();
       });
     } catch (error) {
       print('Erro ao excluir livro: $error');
@@ -40,95 +56,67 @@ class _LivrosScreenState extends State<LivrosScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Lista de Livros'),
-          bottom: TabBar(
-            tabs: [
-              Tab(text: 'Todos'),
-              Tab(text: 'Favoritos'),
-            ],
-          ),
-        ),
-        body: FutureBuilder<List<Livro>>(
-          future: livros,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            }
+    return Scaffold(
+      appBar: AppBar(title: Text('Lista de Livros')),
+      body: FutureBuilder<List<Livro>>(
+        future: livros,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
 
-            if (snapshot.hasError) {
-              return Center(child: Text('Erro: ${snapshot.error}'));
-            }
+          if (snapshot.hasError) {
+            return Center(child: Text('Erro: ${snapshot.error}'));
+          }
 
-            if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return Center(child: Text('Nenhum livro encontrado.'));
-            }
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('Nenhum livro encontrado.'));
+          }
 
-            final allLivros = snapshot.data!;
-            final favoritos =
-                allLivros.where((livro) => livro.isFavorito).toList();
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              final livro = snapshot.data![index];
+              final autorNome = _getAutorNome(livro.autorId);
 
-            return TabBarView(
-              children: [
-                _buildListView(allLivros),
-                _buildListView(favoritos),
-              ],
-            );
-          },
-        ),
-      ),
-    );
-  }
-
-  Widget _buildListView(List<Livro> livros) {
-    return ListView.builder(
-      itemCount: livros.length,
-      itemBuilder: (context, index) {
-        final livro = livros[index];
-        return Card(
-          child: ListTile(
-            title: Text(livro.titulo),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(livro.descricao),
-                Text('Nota: ${livro.nota}'),
-                Text('AutorId: ${livro.autorId}'),
-              ],
-            ),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: Icon(
-                    livro.isFavorito ? Icons.favorite : Icons.favorite_border,
-                    color: livro.isFavorito ? Colors.red : null,
+              return Card(
+                child: ListTile(
+                  title: Text(livro.titulo),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(livro.descricao),
+                      Text('Nota: ${livro.nota}'),
+                      Text('Autor: $autorNome'),
+                    ],
                   ),
-                  onPressed: () => _toggleFavorito(livro),
-                ),
-                IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => FormularioLivroScreen(livro: livro),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) =>
+                                  FormularioLivroScreen(livro: livro),
+                            ),
+                          );
+                        },
                       ),
-                    );
-                  },
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () => _deleteLivro(livro.id),
+                      ),
+                    ],
+                  ),
                 ),
-                IconButton(
-                  icon: Icon(Icons.delete),
-                  onPressed: () => _deleteLivro(livro.id),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
--- test/widgetTest/livro_form_test.dart
+++ test/widgetTest/livro_form_test.dart
@@ -5,28 +5,21 @@ import 'package:flutter_application_1/screens/dashboard_screen.dart';
 void main() {
   testWidgets('Teste de renderização da tela Dashboard',
       (WidgetTester tester) async {
-    // Construir o widget
     await tester.pumpWidget(MaterialApp(
       home: DashboardScreen(),
     ));
 
-    // Abrir o Drawer
-    await tester.tap(find.byIcon(Icons.menu)); // Ação para abrir o Drawer
-    await tester.pumpAndSettle(); // Esperar a animação do Drawer
+    await tester.tap(find.byIcon(Icons.menu));
+    await tester.pumpAndSettle();
 
-    // Verificar se o título da AppBar está presente
     expect(find.text('Dashboard'), findsOneWidget);
 
-    // Verificar se o texto "Bem-vindo à Biblioteca" está presente
     expect(find.text('Bem-vindo à Biblioteca'), findsOneWidget);
 
-    // Verificar se o item "Lista de Livros" está presente no Drawer
     expect(find.text('Lista de Livros'), findsOneWidget);
 
-    // Verificar se o item "Adicionar Livro" está presente no Drawer
     expect(find.text('Adicionar Livro'), findsOneWidget);
 
-    // Verificar se o item "Adicionar Autor" está presente no Drawer
     expect(find.text('Adicionar Autor'), findsOneWidget);
   });
 }
